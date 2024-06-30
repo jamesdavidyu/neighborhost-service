3ee32a5bcb35c4a37cf4e0e4cd1c7d1ec39dd02b
You Young June <youngjune@planorban.com>
2024-06-30 17:39:10 -0400

    starting to build out

---
+++ Makefile
@@ -0,0 +1,17 @@
+build:
+	@go build -o bin/neighborhost-service cmd/main.go
+
+test:
+	@go test -v ./...
+
+run: build
+	@./bin/neighborhost-service
+
+migration:
+	@migrate create -ext sql -dir cmd/migrate/migrations -seq $(filter-out $@,$(MAKECMDGOALS))
+
+migrate-up:
+	@go run cmd/migrate/main.go up
+
+migrate-down:
+	@go run cmd/migrate/main.go down
---
+++ db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+func DB() (*sql.DB, error) {
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	var greeting string
+	err = db.QueryRow("SELECT 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
+	return db, nil
+}
